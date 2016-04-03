0c3f4322016030e18b44eb60fabb28ef05e9a6da
juliannieb <juliannieb@gmail.com>
2016-04-03 14:30:16 -0500

    Add seleccion oficial for an event

--- docu/views.py
+++ docu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from datetime import datetime, timedelta
@@ -141,3 +141,61 @@ def evento(request, festival_id):
 	festivales = Festival.objects.all()
 	context['festivales'] = festivales
 	return render(request, 'evento.html', context)
+
+def seleccion_oficial(request, evento_id):
+	"""Seleccion oficial
+	"""
+	context = {}
+	evento = get_object_or_404(Evento, pk=evento_id)
+	seleccion_oficial = Pelicula.objects.filter(evento=evento.pk, es_seleccion_oficial=True, nacional=True)
+	paginator = Paginator(seleccion_oficial, 24)
+	page = request.GET.get('page')
+	try:
+		seleccion_oficial = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		seleccion_oficial = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		seleccion_oficial = paginator.page(paginator.num_pages)
+	festivales = Festival.objects.all()
+	context['festivales'] = festivales
+	context['evento'] = evento
+	context['seleccion_oficial'] = seleccion_oficial
+	return render(request, 'seleccion_oficial.html', context)
+
+def seleccion_oficial_filtro(request, evento_id, tipo_seleccion):
+	"""Seleccion oficial
+	"""
+	context = {}
+	evento = get_object_or_404(Evento, pk=evento_id)
+	if (tipo_seleccion == 'nacional'):
+		seleccion_oficial = Pelicula.objects.filter(evento=evento.pk, es_seleccion_oficial=True, nacional=True)
+	else:
+		seleccion_oficial = Pelicula.objects.filter(evento=evento.pk, es_seleccion_oficial=True, nacional=False)
+	paginator = Paginator(seleccion_oficial, 24)
+	page = request.GET.get('page')
+	try:
+		seleccion_oficial = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		seleccion_oficial = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		seleccion_oficial = paginator.page(paginator.num_pages)
+	festivales = Festival.objects.all()
+	context['festivales'] = festivales
+	context['evento'] = evento
+	context['seleccion_oficial'] = seleccion_oficial
+	context['origen_seleccion_oficial'] = tipo_seleccion
+	return render(request, 'seleccion_oficial_filtro.html', context)
+
+
+def pelicula(request, pelicula_id):
+	"""Patrocinadores generales de DocumentaQro
+	"""
+	context = {}
+	pelicula = get_object_or_404(Pelicula, pk=pelicula_id)
+	festivales = Festival.objects.all()
+	context['festivales'] = festivales
+	return render(request, 'pelicula.html', context)
