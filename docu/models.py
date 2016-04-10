d96de630eaf8fb6779d6b0c9d40a6da478831ddb
juliannieb <juliannieb@gmail.com>
2016-04-10 12:02:52 -0500

    Add acreditaciones text

--- docu/models.py
+++ docu/models.py
@@ -67,6 +67,8 @@ class Evento(models.Model):
 	fecha_fin = models.DateTimeField('Fecha fin')
 	link_acreditaciones = models.CharField(max_length=2000, null=True, blank=True)
 
+	texto_introductorio_premios = models.TextField(null=True, blank=True)
+
 	def __str__(self):
 		return self.nombre
 
