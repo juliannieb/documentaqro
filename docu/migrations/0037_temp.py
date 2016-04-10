00a6fb7a136b03c7b393ad0d51dae3a35560e94a
juliannieb <juliannieb@gmail.com>
2016-04-09 22:50:20 -0500

    Fix temp migration

--- docu/migrations/0037_temp.py
+++ docu/migrations/0037_temp.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='evento',
-            name='texto_introductorio_seleccion_oficial_nacional',
-            field=models.TextField(blank=True, null=True),
+            name='link_acreditaciones',
+            field=models.CharField(max_length=2000, null=True, blank=True),
         ),
     ]
